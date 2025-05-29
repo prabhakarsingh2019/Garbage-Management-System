fb9499a9bc9d8abbeb823761b61c7cc74e5a2a55
Prabhakar Singh <prabhakarsingh8722@gmail.com>
2025-05-29 21:50:00 +0530

    fix

--- garbage-management-frontend/src/services/routeService.js
+++ garbage-management-frontend/src/services/routeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/routes";
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}/api/routes`;
 
 // Create axios instance with default config
 const apiClient = axios.create({
