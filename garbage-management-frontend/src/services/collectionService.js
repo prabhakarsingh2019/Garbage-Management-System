aba339f9755862a41fd3895077c2280a430cf2e0
Prabhakar Singh <prabhakarsingh8722@gmail.com>
2025-05-29 22:04:28 +0530

    fix

--- garbage-management-frontend/src/services/collectionService.js
+++ garbage-management-frontend/src/services/collectionService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${process.env.REACT_APP_API_BASE_URL}http://localhost:5000/api/collections`;
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}/http://localhost:5000/api/collections`;
 
 const getAllCollections = async (token) => {
   const config = {
